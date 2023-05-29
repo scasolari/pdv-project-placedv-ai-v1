31a6e8d9936f3be05f2e80de30b0647f52f85f8d
sergio.casolari <sergio.casolari@placedv.com>
2023-05-29 17:19:31 +0200

    removed GA from development env

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,25 @@
 import '../styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
 import Script from "next/script";
+import {useEffect} from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    console.log(process.env.NODE_ENV)
+  }, [])
   return <>
-    <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=5271653479`}/>
-    <Script
-        id=""
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      {process.env.NODE_ENV === "development"
+          ? <>
+            <Component {...pageProps} />
+            <Analytics />
+          </>
+          : <>
+            <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=5271653479`}/>
+            <Script
+                id=""
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                  __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -17,9 +27,11 @@ export default function App({ Component, pageProps }) {
               page_path: window.location.pathname,
             });
           `,
-        }}
-    />
-    <Component {...pageProps} />
-    <Analytics />
+                }}
+            />
+            <Component {...pageProps} />
+            <Analytics />
+          </>
+      }
   </>
 }
