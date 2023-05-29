25e43fbcad7edbf6e41c6033f31536847ab197ed
sergio.casolari <sergio.casolari@placedv.com>
2023-05-29 18:13:46 +0200

    added GTM

--- pages/_app.js
+++ pages/_app.js
@@ -14,21 +14,6 @@ export default function App({ Component, pageProps }) {
             <Analytics />
           </>
           : <>
-            <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=5271653479`}/>
-            <Script
-                id=""
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                  __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-8W1GB5H4DP', {
-              page_path: window.location.pathname,
-            });
-          `,
-                }}
-            />
             <Component {...pageProps} />
             <Analytics />
           </>
