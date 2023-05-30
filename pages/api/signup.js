5392b3e554b190163714412c5a9a50f560fd84cb
sergio.casolari <sergio.casolari@placedv.com>
2023-05-30 17:50:52 +0200

    fix

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,30 +1,47 @@
 import axios from 'axios'
+import { render } from '@react-email/render';
+import nodemailer from 'nodemailer';
+import Email from "@/react-email-starter/emails";
 
 export default async function (req, res){
     const { email } = req.body
 
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'info@placedv.com',
+            pass: process.env.GMAIL_PASSWORD,
+        },
+    });
+
+    const emailHtml = render(<Email url="https://example.com" />);
+
+    const email_options = {
+        from: 'info@placedv.com',
+        to: email,
+        subject: 'Thank You for Subscribing to the Placedv AI Newsletter!',
+        html: emailHtml,
+    };
+
     if (!email || !email.length) {
         return res.status(400).json({ error: 'Email is required' })
     }
-
     const API_KEY = process.env.MAILCHIMP_API_KEY
     const API_SERVER = process.env.MAILCHIMP_API_SERVER
     const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID
-
     const url = `https://${API_SERVER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`
-
     const data = {
         email_address: email,
         status: 'subscribed'
     }
-
     const options = {
         headers: {
             'Content-Type': 'application/json',
             Authorization: `api_key ${API_KEY}`
         }
     }
-
     try {
         const response = await axios.post(url, data, options)
         if (response.status >= 400) {
@@ -32,6 +49,7 @@ export default async function (req, res){
                 error: `There was an error subscribing to the newsletter.`
             })
         }
+        transporter.sendMail(email_options);
         return res.status(201).json({ message: 'success' })
     } catch (error) {
         console.log(error)
