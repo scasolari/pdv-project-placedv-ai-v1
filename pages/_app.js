00bd7ad7b33dff31abe1e6158902cf97ebf6d359
sergio.casolari <sergio.casolari@placedv.com>
2023-07-03 10:15:22 +0200

    removed sessions from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 import '../styles/globals.css'
-import { Analytics } from '@vercel/analytics/react';
 import {useEffect} from "react";
-import {SessionProvider} from "next-auth/react";
 
 export default function App({Component, pageProps: { session, ...pageProps }}) {
   useEffect(() => {
     console.log(process.env.NODE_ENV)
   }, [])
   return <>
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-    <Analytics />
+    <Component {...pageProps} />
   </>
 }
